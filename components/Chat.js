4de664e50c1dcbb938d039ad05ff45b017eeb5d2
serinhanw <79671093+serinhanw@users.noreply.github.com>
2021-12-20 11:18:01 -0800

    chat ui w. gitedchat + accessibility

--- components/Chat.js
+++ components/Chat.js
@@ -1,25 +1,95 @@
-import React from 'react';
-import { StyleSheet, View, ImageBackground } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Button, ImageBackground, Image, Platform, KeyboardAvoidingView, } from 'react-native';
+import { Bubble, GiftedChat } from "react-native-gifted-chat";
+
 
 export default class Chat extends React.Component {
-  componentDidMount() {
-    let name = this.props.route.params.name;
-    // Displays the user’s name in the navigation bar at the top of the chat screen.
-    this.props.navigation.setOptions({ title: name });
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
   }
-  render() {
-    const { bgImage } = this.props.route.params;
+
+  componentDidMount() {
+    const { name } = this.props.route.params;
+    // Displays the user’s name in the navigation bar at the top of the chat screen.
+    this.props.navigation.setOptions({ title: name ? name : "Anonymous" });
+    // Sets the state with a static message so that you’ll be able to see 
+    // each element of the UI displayed on screen right away
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hey!",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "Dwight",
+            avatar: "https://m.media-amazon.com/images/I/714oQu1QKIL._AC_SL1500_.jpg",
+          },
+        },
+        // Below is a system message
+        {
+          _id: 2,
+          text: `${name ? name : "Anonymous"} entered the room 👋`,
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
+  }
+
+  // It will be called when a user sends a message.
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  // Create a function to establish bubble color
+  renderBubble(props) {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: this.props.route.params.colorSelectionBackground }}>
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#2f2f2fb8",
+          },
+          left: {
+            backgroundColor: "#ffffffd9",
+          },
+        }}
+      />
+    );
+  }
+
+  render() {
+    const { bgColor, bgImage } = this.props.route.params;
+    return (
+      <View style={{ flex: 1, backgroundColor: bgColor ? bgColor : "#fff" }} >
         <ImageBackground
           source={bgImage}
           resizeMode="cover"
           style={styles.bgImage}
-        />
+        >
+          <GiftedChat
+            renderBubble={this.renderBubble.bind(this)}
+            messages={this.state.messages}
+            onSend={(messages) => this.onSend(messages)}
+            user={{
+              _id: 1,
+            }}
+          />
+          {/* This, prevents the keyboard to hide the message input field, so that you can’t see what you’re typing: */}
+          {Platform.OS === "android" ? <KeyboardAvoidingView behavior="height" /> : null}
+        </ImageBackground>
       </View>
-    )
+    );
   }
 }
+
+
 
 
 const styles = StyleSheet.create({
